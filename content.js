ca1d835a15d70189f5e6b525c3e87880b5806709
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2020-10-05 22:17:19 +0200

    performance enhancement and bug fixes

--- content.js
+++ content.js
@@ -1,11 +1,33 @@
+//handle the video events
+
+document.addEventListener("DOMContentLoaded",function(){
+    alert("loaded");
+})
+
 var vid = document.getElementsByTagName('video')[0];
+
+
 
 vid.addEventListener('progress',function(){
     chrome.runtime.sendMessage('progress');
     // console.log('progress');
-});
+});    
+
+
 
 vid.addEventListener('ended',function(){
     chrome.runtime.sendMessage('ended');
     // console.log('ended');
+});
+
+
+vid.addEventListener('pause',function(){
+    chrome.runtime.sendMessage('pause');
+    // console.log("pause");
+});
+
+
+vid.addEventListener('play',function(){
+    chrome.runtime.sendMessage('play');
+    // console.log("play");
 });
