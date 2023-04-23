c52350871c57e932cd1493f7d78aa12f0cc5f328
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-04-23 03:23:14 +0200

    fixed a bug in the saveWeek function

--- src/javascript/test.js
+++ src/javascript/test.js
@@ -1,26 +1,20 @@
 import { getWeekTotal } from "./modules/getTotal.js";
-import { msToDays } from "./modules/millisFormatting.js";
+import { msToBadgeFormat, msToDays, msToM, msToS } from "./modules/millisFormatting.js";
 import { sortDay, sortWeek } from "./modules/sorting.js";
 
 chrome.alarms.create('oneMinuteAlarm', { periodInMinutes: 1 });
 let save_day = async (prev_day) => {
     let { time_table, week } = await chrome.storage.local.get({ 'time_table': null, 'week': {} })
-    console.log('save day week:', week)
     week[prev_day] = time_table;
-    console.log('save day week after modification:', week)
     await chrome.storage.local.set({ 'prev_day': (new Date()).toDateString(), week });
     await chrome.storage.local.remove('time_table');
-    console.log('saved week', week);
 }
 
 let newDayCheck = async () => {
     let { prev_day } = await chrome.storage.local.get({ 'prev_day': null });
-    console.log('prev_day', prev_day);
-    console.log('today', (new Date()).toDateString());
     if (prev_day) {
         let today = (new Date()).toDateString();
         if (prev_day != today) {
-            console.log('trying to save day');
             await save_day(prev_day);
         }
     } else {
@@ -34,12 +28,12 @@ let newWeekCheck = async () => {
     let { week, prev_sunday } = await chrome.storage.local.get({ 'week': null, 'prev_sunday': null });
     let week_passed = false;
     if (prev_sunday) {
-
+        prev_sunday = new Date(prev_sunday);
         let week_diff_days = Math.floor(msToDays(new Date() - prev_sunday));
         if (week_diff_days >= 7)
             week_passed = true;
-    } else {
-        const [[oldest_day,]] = sortWeek(week).slice(-1);
+    } else if (week) {
+        const [[oldest_day,]] = Object.keys(sortWeek(week)).slice(-1);
         const oldest_date = new Date(oldest_day);
         let week_diff_days = Math.floor(msToDays(new Date() - oldest_date));
         if (week_diff_days < 7 && (oldest_date.getDay() > new Date().getDay())) {
@@ -54,7 +48,7 @@ let newWeekCheck = async () => {
             let [min_day, max_day] = [new Date(Math.min.apply(null, week_dates)), new Date(Math.max.apply(null, week_dates))];
             let { history } = await chrome.storage.local.get({ 'history': {} });
             history[`${min_day.toDateString()} - ${max_day.toDateString()}`] = { week, total: getWeekTotal(week) };
-            chrome.storage.local.set({ history, 'week2': week, 'prev_sunday': new Date() });
+            chrome.storage.local.set({ history, 'week2': week, 'prev_sunday': new Date().toDateString() });
             chrome.storage.local.remove('week');
         }
     } else {
@@ -62,14 +56,13 @@ let newWeekCheck = async () => {
     }
 }
 
-chrome.alarms.onAlarm.addListener(async (alarm) => {
-    console.log('alarm here');
-    console.log('alarm', alarm);
+const handleAlarm = async (alarm) => {
     calculateTime();
     newDayCheck();
     newWeekCheck();
+}
 
-});
+chrome.alarms.onAlarm.addListener(handleAlarm);
 
 //on page updated, tab changed, state is idle/locked, or window is back in focus
 let calculateTime = async () => {
@@ -107,12 +100,7 @@ let calculateTime = async () => {
             console.log('badge updated');
 
             let now = new Date();
-            console.log('now in ms', now.getTime());
-            console.log('now', now);
 
-
-
-            console.log('diff', (new Date).getTime() - prev_url.time);
             let prev_tab_url;
             try {
                 prev_tab_url = new URL(prev_url.url);
@@ -128,7 +116,7 @@ let calculateTime = async () => {
                 console.log('ERROR', e);
             }
         } else {
-            console.log("idle");
+            console.log("idle", { idle_state }, { vid_status });
         }
         prev_url = (tab ? { 'url': tab.url, 'time': (new Date()).getTime() } : null);
         console.log('prev_url before set', prev_url);
