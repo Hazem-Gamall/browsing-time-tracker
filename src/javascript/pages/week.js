cdc83be6a146f2bedabc3de2a62d643090fd8a2b
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-04-24 20:06:14 +0200

    renderWeekRelativeHistory now uses store instead of the chrome storage api

--- src/javascript/pages/week.js
+++ src/javascript/pages/week.js
@@ -6,16 +6,18 @@ import {
     getFormattedWeekDiffPercentage, getWeekDiffPercentage,
     getPercentageArrowIconSpan
 } from '../modules/weekDiffPercentage.js';
+import { store } from '../store/store.js';
 
 
 export function renderWeekRelativeToHistory() {
 
     (async () => {
-        const { week, history } = await chrome.storage.local.get({ week: null, history: null });
+        const week = await store.week;
+        const history = await store.history
         if (!week || !history) {
             document.querySelector("#relative-to-last-week").append("no enough data yet");
         } else {
-            const [{ total }] = Object.values(sortHistory(history)).slice(0, 1);
+            const [{ total }] = Object.values(history).slice(0, 1);
             const percentage = getWeekDiffPercentage(getWeekTotal(week), total);
             const p = document.createElement("p");
             p.append(getPercentageArrowIconSpan(percentage), getFormattedWeekDiffPercentage(percentage));
