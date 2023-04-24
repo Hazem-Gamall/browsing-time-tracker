0726d5c0ebd62b7e658af5f1ba450d25bf896fd3
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2023-04-24 17:58:15 +0200

    changed the name of the contet script

---
+++ src/javascript/content/BrowsingTimeTrackerContent.js
@@ -0,0 +1,32 @@
+//handle the video events
+document.addEventListener("load",() => {
+    
+})
+
+function definePlayingProperty(){
+    Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+        get: function(){
+            return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+        }
+    })
+}
+
+function checkVideoPlayback(){
+    const videoElement = document.querySelector('video');
+    videoElement.playing ?? definePlayingProperty();
+    return document.querySelector('video').playing ? "play":"paused"; 
+}
+
+function reportVideoStatus(){
+    const status = checkVideoPlayback();
+    console.log(`video ${status}`);
+                try{
+                    chrome.runtime.sendMessage(status);
+                }catch(e){
+                    console.log("Error:", e);
+                }
+    
+}
+
+setInterval(reportVideoStatus, 1000);
+
